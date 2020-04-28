d8c06f7dab1914d271f940254e1bdd9e46209131
Manuel Castellin <manuel@castellinconsulting.com>
2020-04-28 15:10:49 +0100

    chore: a few fixes on the command interface

--- jira/main.py
+++ jira/main.py
@@ -7,13 +7,22 @@ server = os.environ["JIRA_SERVER"]
 jira = JIRA(server, basic_auth=auth)
 
 
+def issue2shortstr(issue):
+    return "({}) {}: {}".format(
+        issue.fields.status.name, issue.key, issue.fields.summary
+    )
+
+
 def issue2str(issue):
     text = "Issue: " + issue.key
     text = text + "\nWeb: " + issue.self
     text = text + "\nSummary: " + issue.fields.summary
-    text = text + "\nDescription: " + issue.fields.description
+    text = text + "\nStatus: " + issue.fields.status.name
     text = text + "\nAssignee: " + issue.fields.assignee.displayName
     text = text + "\nReporter: " + issue.fields.reporter.displayName
+    text = text + "\nDescription: " + issue.fields.description
+    subtasks = "\n".join(list(map(issue2shortstr, issue.fields.subtasks)))
+    text = text + "\nSubtasks: \n" + subtasks + "\n"
     return text
 
 
@@ -21,17 +30,27 @@ def issue2str(issue):
 def cli():
     return
 
-@cli.command('show')
+
+@cli.command("show")
 @click.argument("issue_id")
 def show(issue_id):
     issue = jira.issue(issue_id)
     print(issue2str(issue))
 
-@cli.command('assigned')
-def assigned():
-    jql='assignee=currentUser() AND status != Done'
+
+@cli.command("assigned")
+@click.option(
+    "--filter",
+    default=None,
+    help="An additional filter query to apply to the jira search",
+)
+def assigned(filter):
+    jql = "assignee=currentUser() AND status != Done"
+    if filter:
+        jql = jql + " AND " + filter
+
     for issue in jira.search_issues(jql):
-        print ("{}: {}".format(issue.key, issue.fields.summary))
+        print(issue2shortstr(issue))
 
 
 if __name__ == "__main__":
